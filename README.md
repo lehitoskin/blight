238ab0bc6ef646cf1d19c78b6fa9e5a1c89f5277
Lehi Toskin <lehi_toskin@lavabit.com>
2014-04-05 17:43:21 -0700

    Added neil/charterm

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Blight
 ======
 
 Blight is a Tox client written in Racket that utilizes libtoxcore-racket.
+Blight uses the library neil/charterm for its interface. An ncurses wrapper
+does not current exist and that library was at the top of the search results,
+so it'll have to do until GUI programming begins.
--- blight.rkt
+++ blight.rkt
@@ -1,14 +1,48 @@
 #!/usr/bin/env racket
 #lang racket
 ; blight
-; ncurses-based Tox client - for now
+; charterm-based Tox client - for now
 ; GUI is better, but I'm the derps with GUI
-(require libtoxcore-racket)
+(require libtoxcore-racket ; wrapper
+         db                 ; access db for stored info
+         file/sha1          ; hex-string procedures
+         (planet neil/charterm)) ; character-cell terminal interface
 
-(define my-name "Blight Wizard")
+(let loop ()
+  (with-charterm
+   ;(charterm-clear-screen)
+   (charterm-cursor 10 5)
+   (charterm-display "Hello, ")
+   (charterm-bold)
+   (charterm-display "you")
+   (charterm-normal)
+   (charterm-display ".")
+   (charterm-cursor 1 1)
+   (charterm-display "Press a key...")
+   (let ((key (charterm-read-key)))
+     (charterm-cursor 1 1)
+     (charterm-clear-line)
+     (printf "You pressed: ~S\r\n" key)
+     (if (eq? key #\Q)
+         (displayln "Goodbye!")
+         (loop)))))
+
+; tox stuff
+#|(define my-name "Blight Wizard")
 (define my-status-message "Toxing on Blight")
 (define my-tox (tox_new TOX_ENABLE_IPV6_DEFAULT))
 
-(tox_set_status_message my-tox my-status-message (string-length my-status-message))
+(tox_set_status_message my-tox my-status-message (string-length
+                                                  my-status-message))
 
-(tox_kill my-tox)
+; connect to DHT
+(define dht-address "192.254.75.98")
+(define dht-port 33445)
+(define dht-public-key
+  (hex-string->bytes
+   "A09162D68618E742FFBCA1C2C70385E6679604B2D80EA6E84AD0996A1AC8A074"))
+
+(tox_bootstrap_from_address my-tox dht-address TOX_ENABLE_IPV6_DEFAULT dht-port
+                            dht-public-key)
+
+(tox_kill my-tox)|#
