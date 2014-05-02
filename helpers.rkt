bf7ec751ee8a6e350f531af7970bbb3dfc9c1280
Lehi Toskin <lehi@tosk.in>
2014-05-01 19:38:42 -0700

    Refined regex to work is most cases

--- helpers.rkt
+++ helpers.rkt
@@ -1,7 +1,8 @@
-#lang racket
+#lang racket/gui
 ; helpers.rkt
 ; contains miscellaneous procedures for things
 (provide (all-defined-out))
+(require net/url)
 
 (define http?
   (λ (str)
@@ -19,12 +20,20 @@
         (raise-argument-error 'http? "string?" str))))
 
 ; accepts a string and singles out the hyperlink
+; "you're mom likes https://github.com/ and stuff"
+; -> "https://github.com/"
 (define grab-http
   (λ (str)
-    (first
-     (or (regexp-match #rx"http://" str)
-         (regexp-match #rx"https://" str)
-         (regexp-match #rx"www." str)))))
+    ; incomplete grab after ? (index.php?somethinghere)
+    ; incomplete grab if the URL has - or _
+    (let ((url (regexp-match #px"https?://(\\w*\\.)*\\w*(/\\w*)*(\\?\\w*(#\\w*)?)?" str)))
+      (if (false? url)
+          null
+          (first url)))))
+
+; regex for tox://
+;"^((?P<scheme>tox)://)?((?P<tox_id>[[:xdigit:]]{%i})|(?P<authority_user
+;[[:digit:][:alpha:]]+)(@(?P<authority_host>[[:digit:][:alpha:]]+(\\.[[:digit:][:alpha:]]+)+))?)"
 
 ; checks if a given string is in hexadecimal format
 (define hex?
@@ -46,3 +55,18 @@
         (cond [(and (= (string-length str) 76) (hex? str (string-length str))) #t]
               [else #f])
         (raise-argument-error 'tox-id? "string?" str))))
+
+; takes a given string and makes it all blue 'n' shit
+; uses racket/gui to make it blue (and underlined?)
+; uses net/url to make it clickable?
+(define linkify
+  (λ (str)
+    (let* ((http (grab-http str))
+           (url (if (null? http)
+                    null
+                    (string->url http))))
+      (if (null? url)
+          null
+          #|colorify the string
+          clickify the link|#
+          (printf "~a\n" url)))))
