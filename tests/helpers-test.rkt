b538a8f92e1896e5e04b77668fc15a1c0c0d92bc
Andrew Dudash <andrew@crunchbang>
2014-05-11 21:32:10 -0400

    add www. tests and trailing newline

--- tests/helpers-test.rkt
+++ tests/helpers-test.rkt
@@ -3,12 +3,14 @@
 (require rackunit
          "../helpers.rkt")
 
+(define www-url "www.google.com")
 (define http-url "http://github.com/lehitoskin/blight")
 (define https-url "https://github.com/lehitoskin/blight")
 (define ftp-url "ftp://ftp.funet.fi/")
 
 ; http?
 (check-true (http? http-url))
+(check-true (http? www-url))
 (check-false (http? https-url))
 (check-false (http? ftp-url))
 (check-exn exn:fail:contract?
@@ -17,6 +19,7 @@
 
 ; https?
 (check-false (https? http-url))
+(check-false (http? www-url))
 (check-true (https? https-url))
 (check-false (https? ftp-url))
 (check-exn exn:fail:contract?
@@ -26,6 +29,10 @@
 
 ; grab-http
 (check-equal? (grab-http "My favorite part about using https://www.wikipedia.org/ is the part where I use it.")
+              "https://www.wikipedia.org/")
+(check-equal? (grab-http "holla, www.google.com")
+              "www.google.com")
+(check-equal? (grab-http "https://www.wikipedia.org/ https://www.not-wikipedia.org/")
               "https://www.wikipedia.org/")
 (check-equal? (grab-http "Yo, check out\n this seet\r\n awesome\t\t\t\n\v\r link https://www.wikipedia.org/")
               "https://www.wikipedia.org/")
