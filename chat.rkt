c2486d53e688059f7a542fd9f4b79c0d49d21bee
Lehi Toskin <lehi@tosk.in>
2014-05-14 22:49:04 -0700

    Time to start overriding everything!

--- chat.rkt
+++ chat.rkt
@@ -11,8 +11,8 @@
  # functional, but not as pretty as editor-canvas%
  #
  # issues:
- # - still needs to send when user presses enter
- #   as well as needs functionality for Shift-Enter
+ # - need to reimplement a whole bunch of key events because we're
+ #   overriding this, baby!
  |#
 
 ; create a new top-level window
@@ -33,32 +33,6 @@
 (define chat-clipboard the-clipboard)
 (send chat-clipboard-client add-type "TEXT")
 
-; key event when the user presses Enter
-(define enter-press (new key-event%
-                         [key-code #\return]))
-
-; key event for ctrl+c
-(define copy-press (new key-event%
-                        [key-code #\c]
-                        [control-down #t]))
-
-; key event for ctrl+v
-(define paste-press (new key-event%
-                         [key-code #\v]
-                         [control-down #t]))
-
-; key event for ctrl+x
-(define cut-press (new key-event%
-                       [key-code #\x]
-                       [control-down #t]))
-
-; key event for shift+enter
-; so the user can do a newline without sending
-; the message early
-(define shift-enter-press (new key-event%
-                               [key-code #\return]
-                               [shift-down #t]))
-
 (define chat-text-receive (new text%
                                [line-spacing 1.0]
                                [auto-wrap #t]))
@@ -77,20 +51,106 @@
                             [line-spacing 1.0]
                             [auto-wrap #t]))
 
+; guess I need to override some shit to get the keys just right
+(define custom-editor-canvas%
+  (class editor-canvas%
+    (init-field this-parent
+                this-label
+                this-editor
+                this-style
+                this-wheel-step
+                this-min-height
+                this-vert-margin)
+    ; key event for ctrl+c
+    (define copy-press (new key-event%
+                            [key-code #\c]
+                            [control-down #t]))
+    ; key event for ctrl+v
+    (define paste-press (new key-event%
+                             [key-code #\v]
+                             [control-down #t]))
+    ; key event for ctrl+x
+    (define cut-press (new key-event%
+                           [key-code #\x]
+                           [control-down #t]))
+    ; key event for shift+enter
+    ; so the user can do a newline without sending
+    ; the message early
+    (define shift-enter-press (new key-event%
+                                   [key-code #\return]
+                                   [shift-down #t]))
+    ; TODO:
+    ; ^c
+    ; ^p
+    ; ^x
+    ; ^a
+    ; #\ENTER (?)
+    ; unicode? on-release, not on-char
+    ; wheel-up/wheel-down
+    (define/override (on-char key-event)
+      (let ((key (send key-event get-key-code)))
+        (cond
+          ; ^c
+          ; doesn't work
+          ; need to properly compare key-event with copy-press
+          [(equal? key-event copy-press) (send this-editor
+                                               copy-self-to
+                                               chat-clipboard)
+                                         (displayln "Copy-press!")]
+          [(eqv? key #\return) (unless (string=? (send this-editor get-text) "")
+                                 (send chat-text-receive insert
+                                       (string-append "Me: "
+                                                      (send this-editor get-text)
+                                                      "\n"))
+                                 (send this-editor erase))]
+          [(eqv? key #\backspace) (send this-editor delete)]
+          [(eqv? key #\rubout) (send this-editor delete 0 1)]
+          [(eqv? key 'numpad-enter) (send this-editor insert "\n")]
+          [(eqv? key 'left) (send this-editor move-position 'left)]
+          [(eqv? key 'right) (send this-editor move-position 'right)]
+          [(eqv? key 'up) (send this-editor move-position 'up)]
+          [(eqv? key 'down) (send this-editor move-position 'down)]
+          [(char? key) (send this-editor insert key)])))
+    (super-new
+     [parent this-parent]
+     [label this-label]
+     [editor this-editor]
+     [style this-style]
+     [wheel-step this-wheel-step]
+     [min-height this-min-height]
+     [vert-margin this-vert-margin])))
+
 ; an editor canvas where text% messages will appear
-(define chat-editor-canvas-send (new editor-canvas%
-                                     [parent chat-frame]
-                                     [label "Your message goes here"]
-                                     [editor chat-text-send]
-                                     [style (list 'control-border 'no-hscroll
-                                                  'auto-vscroll)]
-                                     [wheel-step 3]
-                                     [min-height 100]
-                                     [vert-margin 5]
-                                     [enabled #t]))
+(define chat-editor-canvas-send (new custom-editor-canvas%
+                                     [this-parent chat-frame]
+                                     [this-label "Your message goes here"]
+                                     [this-editor chat-text-send]
+                                     [this-style (list 'control-border 'no-hscroll
+                                                       'auto-vscroll)]
+                                     [this-wheel-step 3]
+                                     [this-min-height 100]
+                                     [this-vert-margin 5]))
+
+#|(define chat-tfield-send (new text-field%
+                              [parent chat-frame]
+                              [label ""]
+                              [min-height 80]
+                              [style (list 'single)]
+                              [callback (λ (l e)
+                                          (when (eq? (send e get-event-type)
+                                                     'text-field-enter)
+                                            (let ((text (send l get-value)))
+                                              (unless (string=? text "")
+                                                (send chat-text-receive insert
+                                                      (string-append "Me: "
+                                                                     text
+                                                                     "\n"))
+                                                (send l set-value "")))
+                                            (send enter-press get-key-code)))]))
+(define chat-text-send (send chat-tfield-send get-editor))|#
 
 ; chat-panel for main chat-frame
-(define chat-panel (new horizontal-panel%
+#|(define chat-panel (new horizontal-panel%
                         [parent chat-frame]))
 
 ; uses editor-canvas to draw to canvas
@@ -98,7 +158,7 @@
      [label "Send Message"]
      [callback (λ (button event)
                  ; send to chat-editor-canvas-receive the
-                 ; contents of chat-editor-canvas-send
+                 ; contents of chat-tfield-send
                  (send chat-text-receive insert
                        (string-append "Me: "
                                       (send chat-text-send get-text 0 'eof #t #t)
@@ -112,4 +172,4 @@
                                           "False")
                                       "\n"))|#
                  ; clear chat-editor-canvas-send
-                 (send chat-text-send erase))])
+                 (send chat-text-send erase))])|#
