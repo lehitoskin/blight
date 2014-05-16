d32781326859c1c67ecdde6b75a468c975c75ed6
Lehi Toskin <lehi@tosk.in>
2014-05-16 14:42:49 -0700

    Implemented most obvious keyboard shortcuts

--- chat.rkt
+++ chat.rkt
@@ -1,15 +1,9 @@
 #lang racket/gui
 ; chat.rkt
 ; contains chat-window definitions
-(require "helpers.rkt")
 (provide (all-defined-out))
 
 #|
- # two ways (so far) to go about sending a message, graphically:
- # use editor-canvas%, which is pretty, but not as yet obviously
- # function as text-field%, second is text-field%, which is
- # functional, but not as pretty as editor-canvas%
- #
  # issues:
  # - need to reimplement a whole bunch of key events because we're
  #   overriding this, baby!
@@ -62,56 +56,36 @@
                 this-wheel-step
                 this-min-height
                 this-vert-margin)
-    ; key event for ctrl+c
-    (define copy-press (new key-event%
-                            [key-code #\c]
-                            [control-down #t]))
-    ; key event for ctrl+v
-    (define paste-press (new key-event%
-                             [key-code #\v]
-                             [control-down #t]))
-    ; key event for ctrl+x
-    (define cut-press (new key-event%
-                           [key-code #\x]
-                           [control-down #t]))
-    ; key event for shift+enter
-    ; so the user can do a newline without sending
-    ; the message early
-    (define shift-enter-press (new key-event%
-                                   [key-code #\return]
-                                   [shift-down #t]))
-    (define all-press (new key-event%
-                           [key-code #\a]
-                           [control-down #t]))
     ; TODO:
-    ; ^c
-    ; ^p
-    ; ^x
-    ; ^a
-    ; #\ENTER (?)
-    ; unicode? on-release, not on-char
-    ; wheel-up/wheel-down
+    ; fix 'numpad-enter
+    ; unicode?
+    ; wheel-up/wheel-down(?)
     (define/override (on-char key-event)
-      (let ((key (send key-event get-key-code)))
+      (let ((key (send key-event get-key-code))
+            (control (send key-event get-control-down))
+            (shift (send key-event get-shift-down))
+            (alt (send key-event get-alt-down)))
         (cond
           ; keyboard shortcuts
           ; ^c - copy selected text to clipboard
-          ; doesn't work
-          ; need to properly compare key-event with copy-press
-          #|[(equal? key-event copy-press) (send this-editor
-                                               copy-self-to
-                                               chat-clipboard)
-                                         (displayln "Copy-press!")]
+          [(and (eqv? key #\c) (eq? control #t)) (send this-editor copy)]
+          ; ^v - paste text to text area
+          [(and (eqv? key #\v) (eq? control #t)) (send this-editor paste)]
+          ; ^x - cut selected text
+          [(and (eqv? key #\x) (eq? control #t)) (send this-editor cut)]
           ; ^a - select all text in text area
-          [(equal? key-event all-press) (send this-editor move-position 'end)
-                                        (send this-editor extend-position 0)]|#
+          [(and (eqv? key #\a) (eq? control #t)) (send this-editor move-position 'end)
+                                                 (send this-editor extend-position 0)]
           ; press enter - send the message/clear text area
-          [(eqv? key #\return) (unless (string=? (send this-editor get-text) "")
-                                 (send chat-text-receive insert
-                                       (string-append "Me: "
-                                                      (send this-editor get-text)
-                                                      "\n"))
-                                 (send this-editor erase))]
+          [(and (eqv? key #\return) (eq? shift #f))
+           (unless (string=? (send this-editor get-text) "")
+             (send chat-text-receive insert
+                   (string-append "Me: "
+                                  (send this-editor get-text)
+                                  "\n"))
+             (send this-editor erase))]
+          ; shift-enter adds a newline to the text area
+          [(and (eqv? key #\return) (eq? shift #t)) (send this-editor insert "\n")]
           ; press backspace, delete previous character or selected text
           [(eqv? key #\backspace) (send this-editor delete)]
           ; press delete, delete proceding character or selected text
@@ -127,8 +101,9 @@
           [(eqv? key 'down) (send this-editor move-position 'down)]
           [(eqv? key 'home) (send this-editor move-position 'home)]
           [(eqv? key 'end) (send this-editor move-position 'end)]
-          [(eqv? key 'wheel-up) (send this-editor on-char key-event)]
-          [(eqv? key 'wheel-down) (send this-editor on-char key-event)]
+          ; janky workaround because implementing wheel scrolling is a PITA
+          [(eqv? key 'wheel-up) (send this-editor move-position 'up)]
+          [(eqv? key 'wheel-down) (send this-editor move-position 'down)]
           ; regular key pressed, add to text area
           [(char? key) (send this-editor insert key)])))
     (super-new
@@ -150,46 +125,3 @@
                                      [this-wheel-step 3]
                                      [this-min-height 100]
                                      [this-vert-margin 5]))
-
-#|(define chat-tfield-send (new text-field%
-                              [parent chat-frame]
-                              [label ""]
-                              [min-height 80]
-                              [style (list 'single)]
-                              [callback (λ (l e)
-                                          (when (eq? (send e get-event-type)
-                                                     'text-field-enter)
-                                            (let ((text (send l get-value)))
-                                              (unless (string=? text "")
-                                                (send chat-text-receive insert
-                                                      (string-append "Me: "
-                                                                     text
-                                                                     "\n"))
-                                                (send l set-value "")))
-                                            (send enter-press get-key-code)))]))
-(define chat-text-send (send chat-tfield-send get-editor))|#
-
-; chat-panel for main chat-frame
-#|(define chat-panel (new horizontal-panel%
-                        [parent chat-frame]))
-
-; uses editor-canvas to draw to canvas
-(new button% [parent chat-panel]
-     [label "Send Message"]
-     [callback (λ (button event)
-                 ; send to chat-editor-canvas-receive the
-                 ; contents of chat-tfield-send
-                 (send chat-text-receive insert
-                       (string-append "Me: "
-                                      (send chat-text-send get-text 0 'eof #t #t)
-                                      "\n"))
-                 ; is it an http?/https?
-                 ; useful for link coloring etc.
-                 #|(send chat-text-receive insert
-                       (string-append "HTTP? "
-                                      (if (http? (send chat-text-send get-text 0 'eof #t #t))
-                                          "True"
-                                          "False")
-                                      "\n"))|#
-                 ; clear chat-editor-canvas-send
-                 (send chat-text-send erase))])|#
