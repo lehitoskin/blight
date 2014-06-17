ee9f12340178d5e89f1d344efd4b8a6db35637c6
Lehi Toskin <lehi@tosk.in>
2014-06-17 16:00:10 -0700

    Message splitting up to (* TOX_MAX_MESSAGE_LENGTH 3)

--- chat.rkt
+++ chat.rkt
@@ -339,6 +339,88 @@
                      (send emoji-dialog show #f))])
     #| #################### END EMOJI STUFF #################### |#
     
+    ; procedure to inspect a sending message and split it so it does not
+    ; exceed TOX_MAX_MESSAGE_LENGTH
+    (define split-bytes
+      (λ (msg-bytes)
+        (let ((msg-length (bytes-length msg-bytes)))
+          (define max-length TOX_MAX_MESSAGE_LENGTH)
+          ; check if number of bytes exceeds max by more than twice
+          ; currently only supports up to (* max-length 3) message length
+          ; but that should totally be enough for a single message
+          (cond [(> msg-length (* max-length 2))
+                 (define port (open-input-bytes msg-bytes port))
+                 ; return three values
+                 (values (peek-bytes max-length 0 port)
+                         (peek-bytes max-length max-length port)
+                         (if (zero? (remainder (bytes-length msg-bytes) max-length))
+                             (peek-bytes max-length (* max-length 2) port)
+                             (peek-bytes (remainder (bytes-length msg-bytes) max-length)
+                                         (* max-length 2) port)))]
+                [else (define port (open-input-bytes msg-bytes))
+                      ; return two values
+                      (values (peek-bytes max-length 0 port)
+                              (if (zero? (remainder (bytes-length msg-bytes) max-length))
+                                  (peek-bytes max-length max-length port)
+                                  (peek-bytes (remainder (bytes-length msg-bytes) max-length)
+                                              max-length port)))]))))
+    
+    ; send the message to the editor and then through tox
+    ; assumes msg is already a byte-string
+    (define send-message
+      (λ (editor msg-bytes)
+        ; procedure to send to the editor and to tox
+        (define do-send
+          (λ (byte-str)
+            (send chat-text-receive insert
+                  (string-append "Me [" (get-time) "]: " (bytes->string/utf-8 byte-str) "\n"))
+            (tox_send_message this-tox friend-num msg-bytes (bytes-length byte-str))))
+        (cond [(> (bytes-length msg-bytes) (* TOX_MAX_MESSAGE_LENGTH 2))
+               ; if the message is greater than twice our max length, split it
+               ; into three chunks
+               (define-values (first-third second-third third-third)
+                 (split-bytes msg-bytes))
+               ; send first third
+               (do-send first-third)
+               (do-send second-third)
+               (do-send third-third)
+               ; add messages to history
+               ; obtain our tox id
+               (define my-id-bytes (malloc 'atomic
+                                           (* TOX_FRIEND_ADDRESS_SIZE
+                                              (ctype-sizeof _uint8_t))))
+               (tox_get_address this-tox my-id-bytes)
+               (define my-id-hex
+                 (ptrtox->hextox my-id-bytes TOX_FRIEND_ADDRESS_SIZE))
+               (add-history my-id-hex friend-key (send editor get-text) 1)]
+              [(and (> (bytes-length msg-bytes) TOX_MAX_MESSAGE_LENGTH)
+                    (<= (bytes-length msg-bytes) (* TOX_MAX_MESSAGE_LENGTH 2)))
+               ; if the message is greater than our max length, but less than
+               ; or equal to twice the max length, split it into two chunks
+               (define-values (first-half second-half) (split-bytes msg-bytes))
+               (do-send first-half)
+               (do-send second-half)
+               ; add messages to history
+               ; obtain our tox id
+               (define my-id-bytes (malloc 'atomic
+                                           (* TOX_FRIEND_ADDRESS_SIZE
+                                              (ctype-sizeof _uint8_t))))
+               (tox_get_address this-tox my-id-bytes)
+               (define my-id-hex
+                 (ptrtox->hextox my-id-bytes TOX_FRIEND_ADDRESS_SIZE))
+               (add-history my-id-hex friend-key (send editor get-text) 1)]
+              [else
+               (do-send msg-bytes)
+               ; add message to history
+               ; obtain our tox id
+               (define my-id-bytes (malloc 'atomic
+                                           (* TOX_FRIEND_ADDRESS_SIZE
+                                              (ctype-sizeof _uint8_t))))
+               (tox_get_address this-tox my-id-bytes)
+               (define my-id-hex
+                 (ptrtox->hextox my-id-bytes TOX_FRIEND_ADDRESS_SIZE))
+               (add-history my-id-hex friend-key (send editor get-text) 1)])))
+    
     (define chat-text-send (new text%
                                 [line-spacing 1.0]
                                 [auto-wrap #t]))
@@ -356,7 +438,6 @@
                     this-min-height
                     this-vert-margin)
         ; TODO:
-        ; fix 'numpad-enter
         ; unicode?
         ; wheel-up/wheel-down(?)
         (define/override (on-char key-event)
@@ -378,24 +459,10 @@
               ; press enter - send the message/clear text area
               [(and (eqv? key #\return) (eq? shift #f))
                (unless (string=? (send this-editor get-text) "")
-                 (send chat-text-receive insert
-                       (string-append "Me [" (get-time) "]: "
-                                      (send this-editor get-text)
-                                      "\n"))
-                 (tox_send_message this-tox
-                                   friend-num
-                                   (send this-editor get-text)
-                                   (bytes-length
-                                    (string->bytes/utf-8 (send this-editor get-text))))
-                 ; obtain our tox id
-                 (define my-id-bytes (malloc 'atomic
-                                             (* TOX_FRIEND_ADDRESS_SIZE
-                                                (ctype-sizeof _uint8_t))))
-                 (tox_get_address this-tox my-id-bytes)
-                 (define my-id-hex (ptrtox->hextox my-id-bytes TOX_FRIEND_ADDRESS_SIZE))
-                 (add-history my-id-hex friend-key (send this-editor get-text) 1)
-                 (send this-editor erase)
-                 (send chat-text-send change-style font-size-delta))]
+                 (let ((msg-bytes (string->bytes/utf-8 (send this-editor get-text))))
+                   (send-message this-editor msg-bytes)
+                   (send this-editor erase)
+                   (send this-editor change-style font-size-delta)))]
               ; shift-enter adds a newline to the text area
               [(and (eqv? key #\return) (eq? shift #t)) (send this-editor insert "\n")]
               ; enter on the keypad adds a newline to the text area
