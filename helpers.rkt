7578f66a8f622cb1b83019cf7d1bc7b124942f03
Lehi Toskin <lehi@tosk.in>
2014-11-01 22:59:36 -0700

    Get rid of unused code a la #56

--- helpers.rkt
+++ helpers.rkt
@@ -1,26 +1,24 @@
 #lang racket
 ; helpers.rkt
 ; contains miscellaneous procedures for things
-(provide http?
-         https?
-         grab-http
+(provide ;http?
+         ;https?
+         ;grab-http
          any->bool
          hex-string?
          tox-id?
-         delnode
-         setnode
          get-time)
 
 (require #;net/url
          racket/date)
 
-(define/contract http?
+#;(define/contract http?
   (-> string? boolean?)
   (λ (str)
     (or (string=? (substring str 0 7) "http://")
         (string=? (substring str 0 4) "www."))))
 
-(define/contract https?
+#;(define/contract https?
   (-> string? boolean?)
   (λ (str)
     (string=? (substring str 0 8) "https://")))
@@ -28,7 +26,7 @@
 ; accepts a string and singles out the hyperlink
 ; "you're mom likes https://github.com/ and stuff"
 ; -> "https://github.com/"
-(define/contract grab-http
+#;(define/contract grab-http
   (-> string? (or/c null? string?))
   (λ (str)
     ; incomplete grab after ? (index.php?somethinghere)
@@ -65,41 +63,6 @@
   (λ (str)
     (and (= (string-length str) 76)
          (hex-string? str))))
-
-; procedure to remove a specific node in a list
-(define/contract delnode
-  (-> list? (and/c integer? (negate negative?)) list?)
-  (λ (lst num)
-    (when (>= num (length lst))
-      (raise-range-error 'delnode "list" "given " num lst 0 (sub1 (length lst))))
-    (append (take lst num)
-            (drop lst (add1 num)))))
-
-; procedure to replace a specific node in a list
-(define/contract setnode
-  (-> list? any/c (and/c integer? (negate negative?)) list?)
-  (λ (lst node num)
-    (when (>= num (length lst))
-      (raise-range-error 'setnode "list" "given " num lst 0 (sub1 (length lst))))
-    ;; This will be a bit of a hack due to poor stdlib support. There is a
-    ;; third party library that can handle this, but lethoskin has to be
-    ;; convinced to use it first.
-    ;; http://planet.racket-lang.org/display.ss?package=ralist.plt&owner=dvanhorn
-    ;;
-    ;; There's also a list-set that can be found in the unsafe section of
-    ;; the stdlib, but that part of the library is unsafe.
-    ;;
-    ;; Anyway, lists don't have list-set because vectors are supposed to be
-    ;; used for random access lookup, but vectors don't have vector-set
-    ;; because they aren't supported very well. Lists are also immutable so
-    ;; they have to be converted into a vector before they can be mutated.
-    ;;
-    ;; Forgive me lisp alien, for I have sinned!
-    (define a-vector (list->vector lst))
-    (vector-set! a-vector num node)
-    (vector->list a-vector)))
-    
-                 
 
 ; get the current time formatted to HH:MM:SS
 (define get-time
