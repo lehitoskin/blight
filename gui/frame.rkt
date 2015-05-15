1ae2edfd423b5d0f3eb576399fd797380ec81e2e
Lehi Toskin <lehi@tosk.in>
2015-05-14 23:06:53 -0700

    Fix for #60

--- gui/frame.rkt
+++ gui/frame.rkt
@@ -147,18 +147,19 @@ if people have a similar problem.")
                                 ; broadcast to our friends we've changed our avatar
                                 (dprint-wait "Broadcasting our avatar change to online friends")
                                 (for ([count (hash-count cur-buddies)])
-                                  (when (not (eq? (friend-connection-status my-tox count)
-                                                  'none))
-                                    ; file hash is its ID
-                                    (define-values (filenum file-err)
-                                      (file-send my-tox count
-                                                 'avatar
-                                                 (file-size avatar-file)
-                                                 my-hash
-                                                 (string->bytes/utf-8 avatar-file)))
-                                    (transfers-add! my-tox count filenum
-                                                    my-hash avatar-file
-                                                    (file->bytes avatar-file))))
+                                  (let-values ([(fcs-success fcs-err)
+                                                (friend-connection-status my-tox count)])
+                                    (when (not (eq? fcs-success 'none))
+                                      ; file hash is its ID
+                                      (define-values (filenum file-err)
+                                        (file-send my-tox count
+                                                   'avatar
+                                                   (file-size avatar-file)
+                                                   my-hash
+                                                   (string->bytes/utf-8 avatar-file)))
+                                      (transfers-add! my-tox count filenum
+                                                      my-hash avatar-file
+                                                      (file->bytes avatar-file)))))
                                 (displayln "Done!")]))]))]
                      [else
                       ; avatar is within size parameters, just set it
